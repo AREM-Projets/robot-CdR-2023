45f051ea446606c323300ea269736f4143fd73b3
Zeug555 <sergentzeug@gmail.com>
2023-05-06 18:43:33 +0200

    ajout librairie Evitement

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,18 @@
 #include "MagneticEncodeurs.h"
 #include "Odometrie.h"
 #include "ControleurPID.h"
+#include "Evitement.h"
 
-SPIClass* dev_spi;
-BlocMoteurs* motors;
-
-void setup()
-{
-    dev_spi = new SPIClass(D11, D12, D13);
-    dev_spi->begin();
-    motors = new BlocMoteurs(dev_spi);
-
-    motors->motors_on();
-    motors->commande_vitesses(0.5,0.5,0.5,0.5);
-    delay(1000);
-    motors->motors_stop_low_hiz();
+void setup() {
+    pinMode(D7, OUTPUT); //Trig
+    pinMode(D8, INPUT); //Echo
+    Serial.begin (115200);
 }
 
-void loop()
-{
+void loop() {
+    Capteurs Reseau;
+    Reseau.Trig[0] = D7;
+    Reseau.Echo[0] = D8;
+
+    Serial.println(Reseau.RetourCapteur(0));
 }
