2bd2ae8a7cc0ce4252da099830d984ddd172f924
Zeug555 <sergentzeug@gmail.com>
2023-05-07 21:23:13 +0200

    Ajout Bibliotheque d'interruption pour ultrasons

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,84 @@
 #include "ControleurPID.h"
 #include "Evitement.h"
 
-
-Ultrasonic ultrasonic(D7, D8);
+Ultrasonic ultrasonic(D8, D7);
 int distance;
 
-void setup() {
-    pinMode(D7, OUTPUT); //Trig
-    pinMode(D8, INPUT); //Echo
-    Serial.begin(9600);
+// These define's must be placed at the beginning before #include "STM32TimerInterrupt.h"
+// _TIMERINTERRUPT_LOGLEVEL_ from 0 to 4
+// Don't define _TIMERINTERRUPT_LOGLEVEL_ > 0. Only for special ISR debugging only. Can hang the system.
+// Don't define TIMER_INTERRUPT_DEBUG > 2. Only for special ISR debugging only. Can hang the system.
+#define TIMER_INTERRUPT_DEBUG         0
+#define _TIMERINTERRUPT_LOGLEVEL_     0
 
+#include "STM32TimerInterrupt.h"
+#include "STM32_ISR_Timer.h"
+
+#define TIMER_INTERVAL_MS         100
+#define HW_TIMER_INTERVAL_MS      50
+
+// Depending on the board, you can select STM32 Hardware Timer from TIM1-TIM22
+// For example, F767ZI can select Timer from TIM1-TIM14
+// If you select a Timer not correctly, you'll get a message from ci[ompiler
+// 'TIMxx' was not declared in this scope; did you mean 'TIMyy'? 
+
+// Init STM32 timer TIM1
+STM32Timer ITimer(TIM2);
+
+// Init STM32_ISR_Timer
+// Each STM32_ISR_Timer can service 16 different ISR-based timers
+STM32_ISR_Timer ISR_Timer;
+
+#define TIMER_INTERVAL_0_1S           1000L
+
+void TimerHandler()
+{
+  ISR_Timer.run();
 }
 
-void loop() {
-    
-    distance = ultrasonic.read();
+// In STM32, avoid doing something fancy in ISR, for example complex Serial.print with String() argument
+// The pure simple Serial.prints here are just for demonstration and testing. Must be eliminate in working environment
+// Or you can get this run-time error / crash
+void doingSomething1()
+{
+  distance = ultrasonic.read();
+  Serial.println(distance);
+}
 
-    Serial.print("Distance in CM: ");
-    Serial.println(distance);
 
-    
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+  delay(100);
+
+  Serial.print(F("\nStarting TimerInterruptLEDDemo on ")); Serial.println(BOARD_NAME);
+  Serial.println(STM32_TIMER_INTERRUPT_VERSION);
+  Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
+
+  // Instantiate HardwareTimer object. Thanks to 'new' instanciation, HardwareTimer is not destructed when setup() function is finished.
+  //HardwareTimer *MyTim = new HardwareTimer(Instance);
+
+  // configure pin in output mode
+  pinMode(D8, OUTPUT); //Trig
+  pinMode(D7, INPUT); //Echo
+
+  // Interval in microsecs
+  if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_MS * 1000, TimerHandler))
+  {
+    Serial.print(F("Starting ITimer OK, millis() = ")); Serial.println(millis());
+  }
+  else
+    Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+
+  // Just to demonstrate, don't use too many ISR Timers if not absolutely necessary
+  // You can use up to 16 timer for each ISR_Timer
+  ISR_Timer.setInterval(TIMER_INTERVAL_0_1S,  doingSomething1);
+}
+
+
+void loop()
+{
+
 }
