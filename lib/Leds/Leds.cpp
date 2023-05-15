8f7a40b535f93794e34c2a4fa23a65b7fc1f2376
Kezia <kez.marcou@gmail.com>
2023-05-15 23:48:11 +0200

    a moitie fait

--- lib/Leds/Leds.cpp
+++ lib/Leds/Leds.cpp
@@ -4,22 +4,47 @@ Leds::Leds(uint32_t pin_leds)
 {
     pinMode(pin_leds, OUTPUT);
     pin = pin_leds;
+
+    /* Debug print */
+    if(Serial)
+    {
+        Serial.println("Leds initialised");
+    }
 }
 
 void Leds::On()
 {
     digitalWrite(pin, HIGH);
+
+    /* Debug print */
+    if(Serial)
+    {
+        Serial.println("Led ON");
+    }
 }
 
 void Leds::Off()
 {
     digitalWrite(pin, LOW);
+
+    /* Debug print */
+    if(Serial)
+    {
+        Serial.println("Led OFF");
+    }
 }
 
 void Leds::startTimer(uint32_t time_ms/*  = 90000 */)
 {
     startTime_ms = millis();
     timerLength_ms = time_ms;
+
+    /* Debug print */
+    if(Serial)
+    {
+        Serial.printf("Started timer for Leds with time = %d ms ", time_ms);
+        Serial.println();
+    }
 }
 
 bool Leds::timerEndTest()
